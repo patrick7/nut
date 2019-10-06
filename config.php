44b35999b2e2e05b8e880c352ff37cfb4c3d1d3b
Piotr Synowiec <psynowiec@gmail.com>
2019-10-06 17:47:39 +0200

    #config: update

--- config.php
+++ config.php
@@ -2,9 +2,11 @@
 $config = array(
     'port' => '3493',               /* Port of NUT Server */
     'server' => '127.0.0.1',        /* NUT Server */
-    'ups_name' => 'ups',
+    'ups_name' => 'ups',            /* UPS name configured in ups.conf */
     'title' => 'UPS Web Status',    /* Script title */
     'watt' => '400',                /* UPS capacity in Watt */
     'copyright' => 'velder.li',     /* Copyright */
-    'version' => '0.99.1', 'debug' => 'true',);        /* Version */
-?>
+    'version' => '0.99.1',          /* Version */
+    'debug' => 'true',
+    );
+
